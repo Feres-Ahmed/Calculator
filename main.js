1cf1eff0e55ee681f0b43865c8ece0f7903fd3e9
Fares Ahmed <fa1237793@gmail.com>
2026-05-23 00:35:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+let myD =  document.querySelector(".container");
+let result = document.querySelector(".result");
+let sum = "";
+let all = [0,1,2,3,4,5,6,7,8,9,".","%","+","-","*","/","=","Delet","C"];
+
+for(let i =0; i<all.length; i++){
+  let newD = document.createElement("div");
+  newD.textContent = all[i];
+  myD.appendChild(newD);
+  newD.className = "product-box";
+
+  newD.onclick = function(){
+// بيتحقق انها مش رقم
+    if(newD.textContent !== "=" && newD.textContent !== "Delet" && newD.textContent !== "C"){
+    sum += newD.textContent;
+    };
+
+    // للعمليه الحسابيه
+    if(newD.textContent === "="){
+      sum = eval(sum) ;
+    };
+
+    // بيمسح احر رقم
+    if(newD.textContent === "Delet"){
+      sum = sum.slice(0,-1);
+    };
+
+    // بيمسح كل المحتوى
+    if(newD.textContent === "C"){
+      sum = "";
+    };
+
+    result.textContent= sum;
+  };
+
+
+};
